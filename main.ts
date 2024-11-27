48f5353548af5d3ab3e9cfa9661b8ed5584ea900
AbishegaRajan2299 <148842063+AbishegaRajan2299@users.noreply.github.com>
2024-11-27 18:51:02 +0530

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,24 @@
+import 'hammerjs';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+// if (environment.production) {
+//   enableProdMode();
+
+//   // HACK: Don't log to console in production environment.
+//   // TODO: This can be done in better way using logger service and logger factory.
+//   if(window){
+//     window.console.log = window.console.warn = window.console.info = function(){
+//       // Don't log anything.
+//     };
+//   }
+// }
+import { RecaptchaComponent } from 'ng-recaptcha';
+RecaptchaComponent.prototype.ngOnDestroy = function() {};
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
